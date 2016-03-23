519573d0e1e08025a0d3198c3e76ecf715310f24
anjuli-lewis <anjuli.lewis@gmail.com>
2016-03-23 11:58:33 -0500

    Got the hydrogen finished.

--- src/planetproject/UniverseFrame.java
+++ src/planetproject/UniverseFrame.java
@@ -5,6 +5,7 @@
  */
 package planetproject;
 
+import java.awt.Container;
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
@@ -13,11 +14,22 @@ import javax.swing.Timer;
  * @author anjul_000
  */
 public class UniverseFrame extends JFrame {
+
     private Timer timer;
-    
-    public UniverseFrame () {
-       timer = new Timer(1000, new Universe());
-       timer.start();
+    public static final int FRAME_WIDTH = 768;
+    public static final int FRAME_HEIGHT = 768;
+    private int time;
+
+    public UniverseFrame(int time) {
+        this.time = time;
+        this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container pane = this.getContentPane();
+        Universe panel = new Universe();
+        pane.add(panel);
+        this.timer = new Timer(time, panel);
+        timer.start();
+        this.setVisible(true);
     }
-    
+
 }
