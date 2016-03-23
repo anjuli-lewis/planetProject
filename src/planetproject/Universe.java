0f644b09dea54d12e4339bedf95973b702b76092
anjuli-lewis <anjuli.lewis@gmail.com>
2016-03-23 11:58:29 -0500

    Got the hydrogen finished.

--- src/planetproject/Universe.java
+++ src/planetproject/Universe.java
@@ -7,7 +7,7 @@ import java.awt.Graphics2D;
 import java.awt.Shape;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.geom.Dimension2D;
+import java.awt.geom.AffineTransform;
 import javax.swing.JPanel;
 
 /**
@@ -29,47 +29,37 @@ import javax.swing.JPanel;
  */
 public class Universe extends JPanel implements ActionListener {
 
-    private final double radius;     // radius of universe
+    //private final int radius;     // radius of universe
     private final int N;             // number of bodies
-    private final Body[] orbs;       // array of N bodies
+    private final Bondable[] orbs;       // array of N bodies
 
     public Universe() {
-        radius=10;
-        N=10;
-        orbs=new Body[10];
+        orbs = new Bondable[10];
+        N = orbs.length;
+        //radius=orbs.length*2;
+        double neg = 0;
+        double a1 = 0;
+        double a2 = 0;
+        double b1 = 0;
+        double b2 = 0;
+        for (int i = 0; i < orbs.length; i++) {
+            a1 = Math.random();
+            a2 = Math.random();
+            neg = Math.random();
+            if (neg > 0.5) {
+                a1 = a1 * -1;
+            }
+            neg = Math.random();
+            if (neg > 0.5) {
+                a2 = a2 * -1;
+            }
+            double[] arrA = {a1, a2};
+            double[] arrB = {b1, b2};
+            orbs[i] = new Hydrogen(new Vector(arrA));
+            b1++;
+            b2++;
+        }
     }
-    /**
-     * Reads a file to get the size of the universe, how many bodies are in the
-     * universe and the details of those bodies
-     *
-     * @param fileName the file with the data for the universe
-     */
-    public Universe(String fileName) {
-
-        // the authors' version reads from standard input
-        // our version reads from a file
-        In inputStream = new In(fileName);
-
-        // number of bodies
-        N = inputStream.readInt();
-
-        // the set scale for drawing on screen
-        radius = inputStream.readDouble();
-        // read in the N bodies
-        orbs = new Body[N];
-        for (int i = 0; i < N; i++) {
-            double rx = inputStream.readDouble();
-            double ry = inputStream.readDouble();
-            double vx = inputStream.readDouble();
-            double vy = inputStream.readDouble();
-            double mass = inputStream.readDouble();
-            double[] position = {rx, ry};
-            double[] velocity = {vx, vy};
-            Vector r = new Vector(position);
-            Vector v = new Vector(velocity);
-            orbs[i] = new Body(r, v, mass);
-        } // for
-    } // Universe()
 
     /**
      * Increases the time of the simulation, moving the bodies in the universe
@@ -79,51 +69,62 @@ public class Universe extends JPanel implements ActionListener {
      */
     public void increaseTime(double dt) {
 
-        // initialize the forces to zero
-        Vector[] f = new Vector[N];
-        for (int i = 0; i < N; i++) {
-            f[i] = new Vector(new double[2]);
-        } // for
-
-        // compute the forces
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (i != j) {
-                    f[i] = f[i].plus(orbs[i].forceFrom(orbs[j]));
-                } // if
-            } // for
-        } // for
-
-        // move the bodies
-        for (int i = 0; i < N; i++) {
-            orbs[i].move(f[i], dt);
-        } // for
-    } // increaseTime( double )
-
-    /**
-     * Goes through all the bodies and calls their draw function
-     */
-    public void draw() {
-        
-    } // draw()
+    } // increaseTime( double )*/
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g.create();
+        int w = this.getWidth();
+        int h = this.getHeight();
+        AffineTransform scale = new AffineTransform();
+        AffineTransform translate = new AffineTransform();
+        AffineTransform transform = new AffineTransform();
+        translate.setToTranslation(1, 1);
+        scale.setToScale(w / 2, h / 2);
+        transform.concatenate(scale);
+        transform.concatenate(translate);
         for (int i = 0; i < N; i++) {
-            Shape s=orbs[i].draw(orbs[i].getMass());
-            g2d.draw(s);
-            g2d.fill(s);
+            orbs[i].draw(g2d, transform);
         } // for
-    }
-    @Override
-   public Dimension getPreferredSize() {
-           return new Dimension2D.Double(radius,radius);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
-    }
-} // Universe
+        // move the bodies
+        for (int i = 0; i < N; i++) {
+            orbs[i].move(5);
+            Vector v = orbs[i].getPosition();
+            Vector v2=orbs[i].getVelocity();
+            double x = v.cartesian(0);
+            double y = v.cartesian(1);
+            double x2=v2.cartesian(0);
+            double y2=v2.cartesian(1);
+            if (x < -1 || x > 1) {
+                if (x < -1) {
+                    x = -1;
+                } 
+                else {
+                    x = 1;
+                }
+                x2=x2*-1;
+            }
+            if (y < -1 || y > 1) {
+                if (y < -1) {
+                    y = -1;
+                } 
+                else {
+                    y = 1;
+                }
+                y2=y2*-1;
+            }
+            double[] r = {x, y};
+            double [] r2={x2,y2};
+            Vector n = new Vector(r);
+            Vector n2 = new Vector(r2);
+            orbs[i].setPosition(n);
+            orbs[i].setVelocity(n2);
+            this.repaint();
+        }
+    } // Universe
+}
