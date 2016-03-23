b09c34c5731e9d631ba98318c7f2405740cd45cd
anjuli-lewis <anjuli.lewis@gmail.com>
2016-03-23 14:14:28 -0500

    Breaking the way it's supposed to

--- src/planetproject/Hydrogen.java
+++ src/planetproject/Hydrogen.java
@@ -15,16 +15,8 @@ public class Hydrogen extends Bondable {
     public Hydrogen(Vector p) {
         position=p;
         radius=1;
-        double a1=Math.random();
-        double a2=Math.random();
-        double neg=Math.random();
-        if(neg>.5) {
-            a1=-1*a1;
-        }
-        neg=Math.random();
-        if(neg>.5) {
-            a2=-1*a2;
-        }
+        double a1 = (-1 + (2 * Math.random()) ) / 10;
+        double a2=(-1 + (2 * Math.random()) ) / 10;
         double [] arr={a1,a2};
         velocity=new Vector(arr);
         electrons=1;
