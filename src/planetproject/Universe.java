85122a1c54d03d4b81d1c834c619b686067997ef
anjuli-lewis <anjuli.lewis@gmail.com>
2016-03-23 14:14:36 -0500

    Breaking the way it's supposed to

--- src/planetproject/Universe.java
+++ src/planetproject/Universe.java
@@ -32,32 +32,21 @@ public class Universe extends JPanel implements ActionListener {
     //private final int radius;     // radius of universe
     private final int N;             // number of bodies
     private final Bondable[] orbs;       // array of N bodies
+    private double time;
 
-    public Universe() {
-        orbs = new Bondable[10];
+    public Universe(double time) {
+        orbs = new Bondable[5];
         N = orbs.length;
         //radius=orbs.length*2;
+        this.time = time;
         double neg = 0;
         double a1 = 0;
         double a2 = 0;
-        double b1 = 0;
-        double b2 = 0;
         for (int i = 0; i < orbs.length; i++) {
-            a1 = Math.random();
-            a2 = Math.random();
-            neg = Math.random();
-            if (neg > 0.5) {
-                a1 = a1 * -1;
-            }
-            neg = Math.random();
-            if (neg > 0.5) {
-                a2 = a2 * -1;
-            }
+            a1 = (-1 + (2 * Math.random())) / 100;
+            a2 = (-1 + (2 * Math.random())) / 100;
             double[] arrA = {a1, a2};
-            double[] arrB = {b1, b2};
             orbs[i] = new Hydrogen(new Vector(arrA));
-            b1++;
-            b2++;
         }
     }
 
@@ -93,37 +82,36 @@ public class Universe extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         // move the bodies
         for (int i = 0; i < N; i++) {
-            orbs[i].move(5);
+            orbs[i].move(time);
             Vector v = orbs[i].getPosition();
-            Vector v2=orbs[i].getVelocity();
+            Vector v2 = orbs[i].getVelocity();
             double x = v.cartesian(0);
             double y = v.cartesian(1);
-            double x2=v2.cartesian(0);
-            double y2=v2.cartesian(1);
+            double x2 = v2.cartesian(0);
+            double y2 = v2.cartesian(1);
             if (x < -1 || x > 1) {
                 if (x < -1) {
                     x = -1;
-                } 
-                else {
+                } else {
                     x = 1;
                 }
-                x2=x2*-1;
+                x2 = x2 * -1;
             }
             if (y < -1 || y > 1) {
                 if (y < -1) {
                     y = -1;
-                } 
-                else {
+                } else {
                     y = 1;
                 }
-                y2=y2*-1;
+                y2 = y2 * -1;
             }
             double[] r = {x, y};
-            double [] r2={x2,y2};
+            double[] r2 = {x2, y2};
             Vector n = new Vector(r);
             Vector n2 = new Vector(r2);
             orbs[i].setPosition(n);
             orbs[i].setVelocity(n2);
+            System.out.println();
             this.repaint();
         }
     } // Universe
