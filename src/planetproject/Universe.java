279087a02ec5d3063a81f122c60422ad2538cc83
anjuli-lewis <anjuli.lewis@gmail.com>
2016-03-24 09:35:13 -0500

    Bouncing works.

--- src/planetproject/Universe.java
+++ src/planetproject/Universe.java
@@ -29,7 +29,7 @@ import javax.swing.JPanel;
  */
 public class Universe extends JPanel implements ActionListener {
 
-    //private final int radius;     // radius of universe
+    //private final int diameter;     // diameter of universe
     private final int N;             // number of bodies
     private final Bondable[] orbs;       // array of N bodies
     private double time;
@@ -83,28 +83,33 @@ public class Universe extends JPanel implements ActionListener {
         // move the bodies
         for (int i = 0; i < N; i++) {
             orbs[i].move(time);
+            double diameter=(orbs[i].getRadius()*2)/50;
             Vector v = orbs[i].getPosition();
             Vector v2 = orbs[i].getVelocity();
             double x = v.cartesian(0);
             double y = v.cartesian(1);
             double x2 = v2.cartesian(0);
             double y2 = v2.cartesian(1);
-            if (x < -1 || x > 1) {
+            if (x < -1 || x > 1-diameter) {
                 if (x < -1) {
                     x = -1;
-                } else {
-                    x = 1;
+                } 
+                else if (x>1-diameter){
+                    x = 1-diameter;
                 }
                 x2 = x2 * -1;
             }
-            if (y < -1 || y > 1) {
+            if (y < -1 || y > 1-diameter) {
                 if (y < -1) {
                     y = -1;
-                } else {
-                    y = 1;
+                } 
+                else if (y>1-diameter){
+                    y = 1-diameter;
                 }
                 y2 = y2 * -1;
             }
+
+
             double[] r = {x, y};
             double[] r2 = {x2, y2};
             Vector n = new Vector(r);
